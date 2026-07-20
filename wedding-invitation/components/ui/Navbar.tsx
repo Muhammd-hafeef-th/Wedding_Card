f3e3083e9cc01dd08b3037b8fc191174f948f3c3
Muhammad Hafeef th <thhafeef2@gmail.com>
2026-07-20 17:59:08 +0530

    fix: use const for digits and key in romanize (ESLint prefer-const)

--- wedding-invitation/components/ui/Navbar.tsx
+++ wedding-invitation/components/ui/Navbar.tsx
@@ -9,11 +9,11 @@ interface NavbarProps {
 
 const romanize = (num: number) => {
   if (isNaN(num)) return "";
-  let digits = String(+num).split(""),
+  const digits = String(+num).split(""),
     key = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM",
       "", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC",
-      "", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"],
-    roman = "",
+      "", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"];
+  let roman = "",
     i = 3;
   while (i--)
     roman = (key[+(digits.pop() || 0) + (i * 10)] || "") + roman;
